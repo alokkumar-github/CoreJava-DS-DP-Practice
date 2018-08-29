1645cf7512bf8e0b51e07df551209c2d920df341
akumar215 <alok_kumar@comcast.com>
2018-08-29 20:08:31 +0530

    added

--- CoreJavaPracticeExample/src/conceptORproblem/FindMissingNumberInArray.java
+++ CoreJavaPracticeExample/src/conceptORproblem/FindMissingNumberInArray.java
@@ -57,10 +57,36 @@ public class FindMissingNumberInArray {
 //        	  = a3
         return (x1 ^ x2);
 	}
+	 public static int count = 0;
+	    public static int position = 0;
+	    public static boolean flag = false;
+
+	static void findMissingNumbers(int a[],int position) {
+		if (position == a.length - 1)
+            return;
+
+        for (; position < a[a.length - 1]; position++) {
+
+            if ((a[position] - count) != position) {
+                System.out.println("Missing Number: " + (position + count));
+                flag = true;
+                count++;
+                break;
+            }
+        }
+
+        if (flag) {
+            flag = false;
+            findMissingNumbers(a, position);
+        }
+	}
 	public static void main(String[] args) {
 		int a[] = {1,2,3,5,6,7,8};
 		//naiveSol(a);
-		usingXOR(a);
+		// usingXOR(a);
+		int a1[] = { 0, 1, 2, 3, 4, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 18, 20, 21, 23 };
+
+        findMissingNumbers(a1, position);
 
 	}
 
